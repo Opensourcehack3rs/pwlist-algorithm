4951204e518641ba87932048943ae44c0c6bd2c4
Hacker5preme <54862244+Hacker5preme@users.noreply.github.com>
2020-01-28 23:51:00 +0100

    Version 0.2

--- code/algorithm_data.py
+++ code/algorithm_data.py
@@ -1,23 +1,45 @@
 # Version 0.2
 def string_filtering(standard_info):
-    
-def data_processing(standard_info):
+    from copy import deepcopy
+    standard_info_copy = deepcopy(standard_info)
+    base_list_part = []
+    date_list = []
+    for key in standard_info_copy:
+        if standard_info_copy[key] == "none":
+            del standard_info[key]
+        else:
+            element = str(standard_info[key])
+            element = element.replace(".", "")
+            element = element.replace(" ", "")
+            element = element.replace("/", "")
+            if "day" in str(key):
+                year = element[-2:]
+                date = element[:-4]
+                yeardate = date + year
+                date_list.append(element)
+                date_list.append(year)
+                date_list.append(date)
+                date_list.append(yeardate)
+                date_list.append(element[-4:])
+            base_list_part.append(element)
+    base_list = base_list_part + date_list
+    return base_list
+
+
+def data_processing(base_list):
     import itertools
+    end_list = []
     file = open("pwlist-created-wordlist.txt", "w")
     file.write("")
     file.close()
     file = open("pwlist-created-wordlist.txt", "a")
-    list1 = []
-    list2 = []
-    for key in standard_info:
-        if standard_info[key] == "none":
-            pass
-        if standard_info[key] ==
-        else:
-            list1.append(standard_info[key])
-    for i in range(1,len(list1)+1):
-        drip = [list(x) for x in itertools.permutations(list1, i)]
-        list2.extend(drip)
-    for element in list2:
+    for i in range(1,len(base_list)+1):
+        drip = [list(x) for x in itertools.permutations(base_list, i)]
+        end_list.extend(drip)
+    for element in end_list:
         string = ''.join(element)
-        file.write(string + "\n")
+        if len(string) <= 12:
+            file.write(string + "\n")
+        else:
+            pass
+    file.close()
--- code/pwlist-algorithm.py
+++ code/pwlist-algorithm.py
@@ -5,6 +5,7 @@ print("*** PWLIST-ALGORITHM BY Opensourcehack3rs ***")
 print("*** STARTING ***")
 
 standard_info = user_inp_information()
-data_processing(standard_info)
+base_list = string_filtering(standard_info)
+data_processing(base_list)
 print("*** WORDLIST CREATED ***")
 
