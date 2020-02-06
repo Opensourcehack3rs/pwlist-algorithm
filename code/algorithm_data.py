acda57c6fcfcbd5d5d39f9c0fd61b28b1b8c8f28
Hacker5preme <54862244+Hacker5preme@users.noreply.github.com>
2020-02-06 09:15:41 +0100

    Added capital_letter_replacement

--- code/algorithm_data.py
+++ code/algorithm_data.py
@@ -26,22 +26,48 @@ def string_filtering(standard_info):
     return base_list
 
 
-def data_processing(base_list, length_limit):
+def data_processing(base_list):
     import itertools
     end_list = []
-    if length_limit == "none":
-        length_limit = 18
     file = open("pwlist-created-wordlist.txt", "w")
     file.write("")
     file.close()
-    file = open("pwlist-created-wordlist.txt", "a")
     for i in range(1, len(base_list) + 1):
         drip = [list(x) for x in itertools.permutations(base_list, i)]
         end_list.extend(drip)
-    print("*** WRITING GENERATED PASSWORDS INTO THE WORDLIST ***")
+    return end_list
+
+def capital_letters_replacement(end_list):
+    big_list = []
+    writing_list = []
+    chars = ['A', 'a', 'B', 'b', 'C', 'c', 'D', 'd', 'E' 'e', 'F', 'f', 'G', 'g', 'H', 'h', 'I', 'i', 'J', 'j',
+                    'K', 'k', 'L', 'l', 'M', 'm', 'N', 'n', 'O', 'o', 'P', 'p', 'Q', 'q', 'R', 'r' ,'S', 's', 'T', 't', 'U',
+                    'u',
+                    'V', 'v', 'W', 'w', 'X', 'x', 'Y', 'y', 'Z', 'z']
     for element in end_list:
+        string = ''.join(element)
+        if len(string ) >= 1:
+            if string[0] in chars:
+                big_list.append(string.capitalize())
+        else:
+            pass
+    writing_list.extend(end_list)
+    for element in big_list:
+        string = ''.join(element)
+        string = string.replace(string[0], chars[chars.index(string[0])+1])
+        writing_list.append(string)
+    return writing_list
+
+
+def writing(writing_list, length_limit):
+    file = open("pwlist-created-wordlist.txt", "a")
+    if length_limit == "none":
+        length_limit = 18
+    print("*** WRITING GENERATED PASSWORDS INTO THE WORDLIST ***")
+    for element in writing_list:
         string = ''.join(element)
         if len(string) <= int(length_limit):
            file.write(string + "\n")
         else:
             pass
+    file.close()
--- code/pwlist-algorithm.py
+++ code/pwlist-algorithm.py
@@ -7,6 +7,9 @@ print("*** STARTING ***")
 standard_info = user_inp_information()
 length_limit = pw_len_limit()
 base_list = string_filtering(standard_info)
-data_processing(base_list, length_limit)
+end_list = data_processing(base_list)
+writing_list = capital_letters_replacement(end_list)
+writing(writing_list, length_limit)
+
 print("*** WORDLIST CREATED ***")
 
