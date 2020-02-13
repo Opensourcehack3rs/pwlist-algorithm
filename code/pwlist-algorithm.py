f77d79cc8bbd3edb92675f10881eba7b28f4ed49
Hacker5preme <54862244+Hacker5preme@users.noreply.github.com>
2020-02-13 09:28:34 +0100

    Version 0.3

--- code/pwlist-algorithm.py
+++ code/pwlist-algorithm.py
@@ -5,9 +5,10 @@ print("*** PWLIST-ALGORITHM BY Opensourcehack3rs ***")
 print("*** STARTING ***")
 
 standard_info = user_inp_information()
+addi_info = user_inp_add_information()
 length_limit = pw_len_limit()
-base_list = string_filtering(standard_info)
-end_list = data_processing(base_list)
+base_list = string_filtering(standard_info, addi_info)
+end_list = data_processing(base_list,length_limit)
 writing_list = capital_letters_replacement(end_list)
 writing(writing_list, length_limit)
 
